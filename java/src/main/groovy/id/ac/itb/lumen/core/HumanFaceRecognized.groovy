8167dd389e40a9c5b6c8a8cdbef4acfd29f32644
Budhi Yulianto <budhiym@gmail.com>
2015-01-22 14:08:27 +0700

    Implementasi Java untuk Neo4j, Refs #9

--- java/src/main/groovy/id/ac/itb/lumen/core/HumanFaceRecognized.groovy
+++ java/src/main/groovy/id/ac/itb/lumen/core/HumanFaceRecognized.groovy
@@ -6,6 +6,7 @@ import com.fasterxml.jackson.annotation.JsonTypeInfo
 import groovy.transform.CompileStatic
 
 /**
+ * A human face has been recognized based on camera image.
  * Created by ceefour on 19/01/15.
  */
 @CompileStatic
@@ -14,9 +15,21 @@ import groovy.transform.CompileStatic
 @JsonSubTypes(@JsonSubTypes.Type(name="HumanFaceRecognized", value=HumanFaceRecognized.class))
 class HumanFaceRecognized {
 
+    /**
+     * The name of the person that was recognized. 
+     */
     String name
+    /**
+     * The minimum point of the bounding box in 3D space. 
+     */
     Vector3 minPoint
+    /**
+     * The maximum point of the bounding box in 3D space. 
+     */
     Vector3 maxPoint
+    /**
+     * The truth value containing strength and confidence of the face recognition result. 
+     */
     SimpleTruthValue truthValue
 
 }
