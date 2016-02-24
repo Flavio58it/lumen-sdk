9eb7f6202b08c5e58eb123433a3a784c06d97820
Hendy Irawan <ceefour666@gmail.com>
2016-02-25 02:06:17 +0700

    PromptTask matcher is working for xs:date. Refs #25

---
+++ java/src/main/java/org/lskk/lumen/core/LumenLocale.java
@@ -0,0 +1,10 @@
+package org.lskk.lumen.core;
+
+import java.util.Locale;
+
+/**
+ * Created by ceefour on 25/02/2016.
+ */
+public class LumenLocale {
+    public static final Locale INDONESIAN = Locale.forLanguageTag("id-ID");
+}
