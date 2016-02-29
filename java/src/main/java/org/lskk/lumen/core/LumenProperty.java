e26ae585f8dd2fe4bc313253d752e81ef9572607
Hendy Irawan <ceefour666@gmail.com>
2016-02-29 13:33:51 +0700

    FactServiceImplWriteTest

---
+++ java/src/main/java/org/lskk/lumen/core/LumenProperty.java
@@ -0,0 +1,30 @@
+package org.lskk.lumen.core;
+
+import com.google.common.base.MoreObjects;
+
+/**
+ * Created by ceefour on 29/02/2016.
+ */
+public enum LumenProperty {
+    rdfs_label("rdfs:label"),
+    skos_prefLabel("skos:prefLabel"),
+    yago_isPreferredMeaningOf("yago:isPreferredMeaningOf"),
+    yago_hasGivenName("yago:hasGivenName"),
+    yago_hasFamilyName("yago:hasFamilyName"),
+    yago_hasGloss("yago:hasGloss");
+
+    private String qName;
+
+    LumenProperty(String qName) {
+        this.qName = qName;
+    }
+
+    public String getQName() {
+        return qName;
+    }
+
+    @Override
+    public String toString() {
+        return qName;
+    }
+}
