65678cc7a88e7e1d51ffdd229783ff659036f867
Hendy Irawan <ceefour666@gmail.com>
2016-03-19 00:27:55 +0700

    audio.ts

--- mobile/src/audio.ts
+++ mobile/src/audio.ts
@@ -1,27 +1,32 @@
 ///<reference path="../typings/main.d.ts"/>
 angular.module('starter.controllers')
-    .controller('AudioMonitorCtrl', function ($scope, $stateParams, $log, $window, Settings, LumenStomp) {
-    $scope.form = {};
+
+.controller('AudioMonitorCtrl', function($scope, $stateParams, $log, $window, Settings, LumenStomp) {
+    $scope.form = {
+    };
+
     $scope.client = null;
-    $scope.$on('$ionicView.enter', function () {
-        LumenStomp.connect(function () {
+    $scope.$on('$ionicView.enter', function() {
+        LumenStomp.connect(function() {
             $scope.client = LumenStomp.getClient();
             // audio.out: AudioObject
-            LumenStomp.subscribe('/topic/avatar.*.audio.out', function (exchange) {
+            LumenStomp.subscribe('/topic/avatar.*.audio.out', function(exchange) {
                 var msg = JSON.parse(exchange.body);
                 $log.info("Received audio", msg.name, msg.contentType, msg.contentSize, 'bytes');
-                document.getElementById('played').src = msg.contentUrl;
+                (document.getElementById('played') as HTMLMediaElement).src = msg.contentUrl;
                 $scope.replayPlayed();
             });
         });
     });
-    $scope.$on('$ionicView.beforeLeave', function () {
+    $scope.$on('$ionicView.beforeLeave', function() {
         LumenStomp.disconnect();
     });
+
     // Audio
-    $scope.replayPlayed = function () {
-        var playedEl = document.getElementById('played');
+    $scope.replayPlayed = function() {
+        var playedEl = document.getElementById('played') as HTMLMediaElement;
         $log.info('Playing played ', playedEl, 'seconds ...');
         playedEl.play();
     };
+
 });
