c655fee267581b9a925cbff82988883192ddc7b2
Hendy Irawan <ceefour666@gmail.com>
2016-03-19 11:09:22 +0700

    Angular 2 style services

--- mobile/src/services.ts
+++ mobile/src/services.ts
@@ -1,72 +1,84 @@
 /// <reference path="../typings/main.d.ts"/>
-angular.module('starter.services', [])
 
-.factory('Settings', function($q, $log, $window) {
-    var defaultSettings = {
+class Settings {
+    public defaultSettings = {
         stompUri: 'ws://167.205.66.35:15674/ws',
         stompUser: 'lumen',
         stompPassword: 'lumen',
         motionAllowed: false,
     };
-    return {
-        getDefault: function() {
-            return defaultSettings;
-        },
-        getSettings: function() {
-            var settingsJson = $window.localStorage.getItem('settings');
-            var settings = settingsJson !== null ? JSON.parse(settingsJson) : defaultSettings;
-            return settings;
-        },
-        setSettings: function(settings) {
-            $window.localStorage.setItem('settings', JSON.stringify(settings));
-            return settings;
-        },
-    };
-})
 
-.factory('LumenStomp', function($q, $log, $window, Settings, ngstomp) {
-    var client = null;
-    var subscriptions = [];
-    return {
-        connect: function(callback) {
-            var settings = Settings.getSettings();
-            $log.info('Stomp connecting to', settings.stompUri);
-            client = ngstomp(settings.stompUri);
-            client.connect(settings.stompUser, settings.stompPassword, function() {
-                $log.info('Stomp connected to', settings.stompUri);
-                callback();
-            }, function(err) {
-                $log.error('Stomp error:', err);
-                client = null;
-            }, '/');
-        },
-        disconnect: function() {
-            this.unsubscribeAll();
-            if (client != null) {
-                $log.info('Disconnecting', client);
-                client.disconnect(function() { $log.info('Disconnected.'); });
-                client = null;
-            }
-        },
-        getClient: function() {
-            return client;
-        },
-        getSubscriptions: function() {
-            return subscriptions;
-        },
-        subscribe: function(topic, headers, body) {
-            var sub = client.subscribe(topic, headers, body);
-            subscriptions.push(sub);
-            return sub;
-        },
-        unsubscribeAll: function() {
-            _.forEach(subscriptions, function(sub) {
-                $log.info('Unsubscribing', sub);
-                sub.unsubscribe();
-            });
-            subscriptions = [];
-        },
-    };
-})
+    constructor(public $q, public $log, public $window) {
+    }
 
-;
+    getDefault() {
+        return this.defaultSettings;
+    }
+
+    getSettings() {
+        var settingsJson = this.$window.localStorage.getItem('settings');
+        var settings = settingsJson !== null ? JSON.parse(settingsJson) : this.defaultSettings;
+        return settings;
+    }
+
+    setSettings(settings) {
+        this.$window.localStorage.setItem('settings', JSON.stringify(settings));
+        return settings;
+    }
+}
+
+class LumenStomp {
+    public client = null;
+    public subscriptions = [];
+    
+    constructor(public $q, public $log, public $window, public Settings: Settings, public ngstomp) {
+    }
+    
+    connect(callback) {
+        var settings = this.Settings.getSettings();
+        this.$log.info('Stomp connecting to', settings.stompUri);
+        this.client = this.ngstomp(settings.stompUri);
+        this.client.connect(settings.stompUser, settings.stompPassword, () => {
+            this.$log.info('Stomp connected to', settings.stompUri);
+            callback();
+        }, err => {
+            this.$log.error('Stomp error:', err);
+            this.client = null;
+        }, '/');
+    }
+    
+    disconnect() {
+        this.unsubscribeAll();
+        if (this.client != null) {
+            this.$log.info('Disconnecting', this.client);
+            this.client.disconnect(function() { this.$log.info('Disconnected.'); });
+            this.client = null;
+        }
+    }
+    
+    getClient() {
+        return this.client;
+    }
+    
+    getSubscriptions() {
+        return this.subscriptions;
+    }
+    
+    subscribe(topic, headers, body) {
+        var sub = this.client.subscribe(topic, headers, body);
+        this.subscriptions.push(sub);
+        return sub;
+    }
+    
+    unsubscribeAll() {
+        this.subscriptions.forEach(sub => {
+            this.$log.info('Unsubscribing', sub);
+            sub.unsubscribe();
+        });
+        this.subscriptions = [];
+    }
+}
+
+angular.module('starter.services', [])
+    .service('Settings', Settings)
+    .service('LumenStomp', LumenStomp);
--- mobile/www/js/services.js
+++ mobile/www/js/services.js
@@ -1,70 +1,84 @@
 /// <reference path="../typings/main.d.ts"/>
+var Settings = (function () {
+    function Settings($q, $log, $window) {
+        this.$q = $q;
+        this.$log = $log;
+        this.$window = $window;
+        this.defaultSettings = {
+            stompUri: 'ws://167.205.66.35:15674/ws',
+            stompUser: 'lumen',
+            stompPassword: 'lumen',
+            motionAllowed: false
+        };
+    }
+    Settings.prototype.getDefault = function () {
+        return this.defaultSettings;
+    };
+    Settings.prototype.getSettings = function () {
+        var settingsJson = this.$window.localStorage.getItem('settings');
+        var settings = settingsJson !== null ? JSON.parse(settingsJson) : this.defaultSettings;
+        return settings;
+    };
+    Settings.prototype.setSettings = function (settings) {
+        this.$window.localStorage.setItem('settings', JSON.stringify(settings));
+        return settings;
+    };
+    return Settings;
+}());
+var LumenStomp = (function () {
+    function LumenStomp($q, $log, $window, Settings, ngstomp) {
+        this.$q = $q;
+        this.$log = $log;
+        this.$window = $window;
+        this.Settings = Settings;
+        this.ngstomp = ngstomp;
+        this.client = null;
+        this.subscriptions = [];
+    }
+    LumenStomp.prototype.connect = function (callback) {
+        var _this = this;
+        var settings = this.Settings.getSettings();
+        this.$log.info('Stomp connecting to', settings.stompUri);
+        this.client = this.ngstomp(settings.stompUri);
+        this.client.connect(settings.stompUser, settings.stompPassword, function () {
+            _this.$log.info('Stomp connected to', settings.stompUri);
+            callback();
+        }, function (err) {
+            _this.$log.error('Stomp error:', err);
+            _this.client = null;
+        }, '/');
+    };
+    LumenStomp.prototype.disconnect = function () {
+        this.unsubscribeAll();
+        if (this.client != null) {
+            this.$log.info('Disconnecting', this.client);
+            this.client.disconnect(function () { this.$log.info('Disconnected.'); });
+            this.client = null;
+        }
+    };
+    LumenStomp.prototype.getClient = function () {
+        return this.client;
+    };
+    LumenStomp.prototype.getSubscriptions = function () {
+        return this.subscriptions;
+    };
+    LumenStomp.prototype.subscribe = function (topic, headers, body) {
+        var sub = this.client.subscribe(topic, headers, body);
+        this.subscriptions.push(sub);
+        return sub;
+    };
+    LumenStomp.prototype.unsubscribeAll = function () {
+        var _this = this;
+        this.subscriptions.forEach(function (sub) {
+            _this.$log.info('Unsubscribing', sub);
+            sub.unsubscribe();
+        });
+        this.subscriptions = [];
+    };
+    return LumenStomp;
+}());
 angular.module('starter.services', [])
-    .factory('Settings', function ($q, $log, $window) {
-    var defaultSettings = {
-        stompUri: 'ws://167.205.66.35:15674/ws',
-        stompUser: 'lumen',
-        stompPassword: 'lumen',
-        motionAllowed: false
-    };
-    return {
-        getDefault: function () {
-            return defaultSettings;
-        },
-        getSettings: function () {
-            var settingsJson = $window.localStorage.getItem('settings');
-            var settings = settingsJson !== null ? JSON.parse(settingsJson) : defaultSettings;
-            return settings;
-        },
-        setSettings: function (settings) {
-            $window.localStorage.setItem('settings', JSON.stringify(settings));
-            return settings;
-        }
-    };
-})
-    .factory('LumenStomp', function ($q, $log, $window, Settings, ngstomp) {
-    var client = null;
-    var subscriptions = [];
-    return {
-        connect: function (callback) {
-            var settings = Settings.getSettings();
-            $log.info('Stomp connecting to', settings.stompUri);
-            client = ngstomp(settings.stompUri);
-            client.connect(settings.stompUser, settings.stompPassword, function () {
-                $log.info('Stomp connected to', settings.stompUri);
-                callback();
-            }, function (err) {
-                $log.error('Stomp error:', err);
-                client = null;
-            }, '/');
-        },
-        disconnect: function () {
-            this.unsubscribeAll();
-            if (client != null) {
-                $log.info('Disconnecting', client);
-                client.disconnect(function () { $log.info('Disconnected.'); });
-                client = null;
-            }
-        },
-        getClient: function () {
-            return client;
-        },
-        getSubscriptions: function () {
-            return subscriptions;
-        },
-        subscribe: function (topic, headers, body) {
-            var sub = client.subscribe(topic, headers, body);
-            subscriptions.push(sub);
-            return sub;
-        },
-        unsubscribeAll: function () {
-            _.forEach(subscriptions, function (sub) {
-                $log.info('Unsubscribing', sub);
-                sub.unsubscribe();
-            });
-            subscriptions = [];
-        }
-    };
-});
+    .service('Settings', Settings)
+    .service('LumenStomp', LumenStomp);
 
 //# sourceMappingURL=services.js.map
