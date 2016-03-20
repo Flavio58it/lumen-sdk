7f80b0921c3efeaccfe00dfcd6319fef5cdf91fa
Hendy Irawan <ceefour666@gmail.com>
2016-03-20 13:23:02 +0700

    angularAudioRecorder

--- mobile/src/app.ts
+++ mobile/src/app.ts
@@ -6,7 +6,8 @@
 // the 2nd parameter is an array of 'requires'
 // 'starter.controllers' is found in controllers.js
 angular.module('starter', ['ionic', 'starter.controllers', 'starter.services',
-    'AngularStomp', 'webcam', 'monospaced.elastic', 'angularMoment'])
+    'AngularStomp', 'webcam', 'monospaced.elastic', 'angularMoment',
+    'angularAudioRecorder'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
